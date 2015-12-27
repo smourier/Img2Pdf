e7c357027318f0723e550e5229dbe759ecb84a2a
Simon Mourier <simon_mourier@hotmail.com>
2015-12-27 17:59:24 +0100

    Initial commit

---
+++ Img2Pdf/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Img2Pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
